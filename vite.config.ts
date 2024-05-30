b87758438c2c7043716b6534cc4323e70e444844
reemo <r48n34@gmail.com>
2024-05-30 21:49:21 +0800

    Added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    drop: ['console', 'debugger'],
-  },
-  build: {
-    sourcemap: false,
-  }
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'vite.svg'],
+            manifest: {
+                name: '学園アイドルマスター Calculator',
+                short_name: '学マス Cal',
+                description: 'Calculator the estimated final exam score',
+                theme_color: '#1a1b1e',
+                icons: [
+                    {
+                      src: 'vite.svg',
+                      sizes: '192x192',
+                      type: 'image/svg'
+                    }
+                ]
+            }
+        })
+    ],
+    esbuild: {
+        drop: ['console', 'debugger'],
+    },
+    build: {
+        sourcemap: false,
+    }
 })
